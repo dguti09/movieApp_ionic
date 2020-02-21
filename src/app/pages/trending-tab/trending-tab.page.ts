b37516eb3f037913451419593d07db81d663d206
Cristhian Rodriguez <cristhian.rodriguez@farmatodo.com>
2020-02-21 16:40:56 -0500

    animation progress call service

--- src/app/pages/trending-tab/trending-tab.page.ts
+++ src/app/pages/trending-tab/trending-tab.page.ts
@@ -14,34 +14,50 @@ export class TrendingTabPage implements OnInit {
     tvIsEnable: boolean;
     eventWeekOrDay = '1';
     showProgress: boolean;
-    selectedColorDay: string;
-    selectedColorWeek: string;
+    selectedColorDay = 'white';
+    selectedColorWeek = 'black';
+    selectedDay = true;
+    selectedWeek = false;
+    private notifications = {
+        value : 'day'
+    };
 
     constructor(private moviesProvider: MoviesMagnamentProvider, private seriesService: SeriesService) {
         this.tvIsEnable = seriesService.tvIsEnableService;
         this.getDay();
-        this.selectedColorDay = 'white'
-        this.selectedColorWeek = 'dark'
+        this.selectedColorDay = 'dark';
+        this.selectedColorWeek = 'dark';
         this.seriesService.componentMethodCalled$.subscribe( () => {
                 console.log('llamado desde el switch');
                 this.showProgress = true;
                 this.eventWeekOrDay === '1' ? this.getDay() : this.getWeek();
             }
         );
+        this.notifications.value = 'day';
     }
 
     changeButton(event) {
         this.movies = [];
         this.showProgress = true;
         if (event.detail.value === '1') {
+            console.log('1');
+            this.selectedDay = true;
+            this.selectedWeek = false;
             this.eventWeekOrDay = '1';
-            this.selectedColorDay = 'white'
-            this.selectedColorWeek = 'dark'
+            this.selectedColorDay = 'black';
+            this.selectedColorWeek = 'white';
+            console.log(' day -> ' ,  this.selectedDay);
+            console.log(' week -> ' ,  this.selectedWeek);
             this.getDay();
         } else if (event.detail.value === '2') {
+            console.log('2');
+            this.selectedDay = false;
+            this.selectedWeek = true;
+            console.log(' day -> ' ,  this.selectedDay);
+            console.log(' week -> ' ,  this.selectedWeek);
             this.eventWeekOrDay = '2';
-            this.selectedColorDay = 'dark'
-            this.selectedColorWeek = 'white'
+            this.selectedColorDay = 'white';
+            this.selectedColorWeek = 'black';
             this.getWeek();
         }
     }
